6e9977898c9635d763808504a90cc3301cda1e29
Sahil Sharma <vlsharma713@gmail.com>
2024-04-15 17:25:02 +0530

    Add download file button

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import get_parsed_resumes
+import utils 
 
 
 st.title('Resume Parser App')
@@ -14,8 +14,28 @@ st.subheader('Upload Resumes')
 uploaded_files = st.file_uploader('Choose files', accept_multiple_files=True, type=['pdf', 'docx', 'doc'])
 st.write('')
 
+excel_created = False
 if uploaded_files:
-    st.success('Files uploaded successfully', icon="✅")
-    resumes = get_parsed_resumes(uploaded_files)
-    resumes    
-    
+    with st.status("Parsing resumes...", expanded=True) as status:
+        st.write("Uploading files...")
+        resumes = utils.get_parsed_resumes(uploaded_files)
+
+        st.write("Getting the parsed data...")
+        sheet, workbook = utils.create_excelsheet()
+
+        st.write("Creating excel sheet...")
+        utils.add_data_to_excelsheet(sheet, workbook, resumes)
+        workbook.save("resumes.xlsx")
+        status.update(label="Excel File Created ", state="complete", expanded=False)
+        excel_created = True
+        
+st.write('')
+if excel_created:
+    with open("resumes.xlsx", "rb") as file:
+        st.download_button(
+            label="Download excel file",
+            data=file,
+            file_name="resumes.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from affinda import AffindaAPI, TokenCredential
+import openpyxl
+import streamlit as st
 
 
 load_dotenv()
@@ -19,3 +21,23 @@ def get_parsed_resumes(files):
         resume = client.create_document(file=file, file_name=file.name, collection=resume_collection.identifier)
         resumes.append(resume.as_dict())
     return resumes
+
+
+def create_excelsheet():
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    # headers = ["Name", "Email", "Phone", "Education", "Work Experience"]
+    headers = ['Name', 'Email', 'Phone']
+    sheet.append(headers)
+    return sheet, workbook
+
+
+def add_data_to_excelsheet(sheet, workbook, resumes):
+    for resume in resumes:
+        parsed_data = resume['data']
+        name = parsed_data['name']['first'] + ' ' + parsed_data['name']['middle']  + parsed_data['name']['last']
+        email = '\n'.join(parsed_data['emails'])
+        phone = '\n'.join(parsed_data['phone_numbers'])
+        sheet.append([name, email, phone])
+    workbook.save("resumes.xlsx")
